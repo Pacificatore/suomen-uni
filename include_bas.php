57fefa9cc06d5c77bfe64852ee34da5e36605043
Your Name <you@example.com>
2022-12-11 00:20:13 +0100

    Ajout des includes php pour le core code

---
+++ include_bas.php
@@ -0,0 +1,97 @@
+<td rowspan="6">
+    <img src="images/Suomen-uni-3_15.png" width="28" height="747" alt="">
+</td>
+<td>
+    <img src="images/spacer.gif" width="1" height="560" alt="">
+</td>
+</tr>
+<tr>
+    <td rowspan="5">
+        <img src="images/Suomen-uni-3_16.png" width="25" height="187" alt="">
+    </td>
+    <td>
+        <img src="images/Suomen-uni-3_17.png" width="52" height="52" alt="">
+    </td>
+    <td rowspan="5">
+        <img src="images/Suomen-uni-3_18.png" width="62" height="187" alt="">
+    </td>
+    <td>
+        <img src="images/spacer.gif" width="1" height="52" alt="">
+    </td>
+</tr>
+<tr>
+    <td>
+        <img src="images/Suomen-uni-3_19.png" width="52" height="35" alt="">
+    </td>
+    <td>
+        <img src="images/spacer.gif" width="1" height="35" alt="">
+    </td>
+</tr>
+<tr>
+    <td rowspan="2">
+        <img src="images/Suomen-uni-3_20.png" width="52" height="52" alt="">
+    </td>
+    <td>
+        <img src="images/spacer.gif" width="1" height="40" alt="">
+    </td>
+</tr>
+<tr>
+    <td colspan="7" rowspan="2">
+        <img src="images/Suomen-uni-3_21.png" width="627" height="60" alt="">
+    </td>
+    <td>
+        <img src="images/spacer.gif" width="1" height="12" alt="">
+    </td>
+</tr>
+<tr>
+    <td>
+        <img src="images/Suomen-uni-3_22.png" width="52" height="48" alt="">
+    </td>
+    <td>
+        <img src="images/spacer.gif" width="1" height="48" alt="">
+    </td>
+</tr>
+<tr>
+    <td>
+        <img src="images/spacer.gif" width="25" height="1" alt="">
+    </td>
+    <td>
+        <img src="images/spacer.gif" width="52" height="1" alt="">
+    </td>
+    <td>
+        <img src="images/spacer.gif" width="62" height="1" alt="">
+    </td>
+    <td>
+        <img src="images/spacer.gif" width="109" height="1" alt="">
+    </td>
+    <td>
+        <img src="images/spacer.gif" width="16" height="1" alt="">
+    </td>
+    <td>
+        <img src="images/spacer.gif" width="127" height="1" alt="">
+    </td>
+    <td>
+        <img src="images/spacer.gif" width="132" height="1" alt="">
+    </td>
+    <td>
+        <img src="images/spacer.gif" width="85" height="1" alt="">
+    </td>
+    <td>
+        <img src="images/spacer.gif" width="49" height="1" alt="">
+    </td>
+    <td>
+        <img src="images/spacer.gif" width="109" height="1" alt="">
+    </td>
+    <td>
+        <img src="images/spacer.gif" width="28" height="1" alt="">
+    </td>
+    <td>
+        <img src="images/spacer.gif" width="6" height="1" alt="">
+    </td>
+    <td></td>
+</tr>
+</table>
+<!-- End Save for Web Slices -->
+</body>
+
+</html>
---
+++ index_test_includes.php
@@ -0,0 +1,46 @@
+<html lang="fr">
+
+<head>
+    <title>Suomen uni</title>
+    <meta charset="utf-8" />
+    <link type="text/css" rel="stylesheet" href="style.css" />
+
+
+
+</head>
+
+
+<?php include('include_haut.php'); ?>
+<p>
+<h3> Bienvenue! Tervetuloa kaikille!</h3>
+</p>
+
+
+<p>Ce site sera dédié principalement à la langue finnoise. Une grammaire pour tous! <br />
+    Voyant peu de sites en français, on a décidé d'en créer un, pour débutants pur, faux débutants,
+    pour les curieux, pour tout le monde. </p>
+
+<p>Comment le site va se présenter? Découvrons le ensemble.<br /></p>
+
+<p>Tout d'abord, la partie la plus importante, celle des cours. Dans le menu cours (ci-dessus), vous
+    trouverez la liste des cours disponibles. Espérant tenir le site en pérpétuelle création, la
+    liste devrait s'allonger au fil du temps. </p>
+
+<p> Quoi de mieux pour tester ses connaissances que des excercices? En effet, des excercices types
+    QCM, textes à trous, etc... vous seront proposés dans la partie "tests". </p>
+
+
+
+<p> Quelques tableaux récapitulatifs seront également présents dans la partie prévue à cet effet.
+</p>
+
+<p> Pour toutes questions, ou autres, rendez vous dans la partie "contactez-nous". On a encore
+    jamais mangé personne.</p>
+
+<p> Bon apprentissage!</p>
+
+
+</div>
+</td>
+
+<?php include('include_bas.php'); ?>
